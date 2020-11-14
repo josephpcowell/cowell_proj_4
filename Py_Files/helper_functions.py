bcefa70db45ff1b8ffbdd89111fe8062e154bea1
Joe Cowell <37605645+josephpcowell@users.noreply.github.com>
2020-11-14 09:51:47 -0500

    Added detailed descriptions

--- Py_Files/helper_functions.py
+++ Py_Files/helper_functions.py
@@ -1,5 +1,5 @@
 """
-These two functions help take twitter data and turn it into a
+These functions help take twitter data and turn it into a
 workable corpus for NLP
 """
 
@@ -9,19 +9,20 @@ import pickle
 import json
 import seaborn as sns
 import matplotlib as plt
+import re
 
 
 def txt_to_df(txt_path):
     """
     Powerhouse function to take raw scraped twitter data
-    into a DataFrame of just the tweet texts
+    into a DataFrame of just the tweet texts and years
 
     Args:
         txt_path: The path for the saved file containing
             the tweet data in json format
 
     Returns:
-        A DataFrame containing just the raw tweet texts
+        A DataFrame containing just the raw tweet texts and years tweeted
     """
     path = txt_path
     tweets_file = open(path, "r")
@@ -53,23 +54,17 @@ def txt_to_df(txt_path):
             return tweet[["long_text", "year"]]
 
 
-def snscrape_to_df(txt_path):
-    path = txt_path
-    tweets_file = open(path, "r")
-    tweets_data = []
-    for line in tweets_file:
-        try:
-            tweet = json.loads(line)
-            tweets_data.append(tweet)
-        except:
-            continue
-    tweet = pd.DataFrame(tweets_data)
-    tweet = tweet[tweet["lang"] == "en"]
-    tweet["long_text"] = tweet["content"]
-    return tweet[["long_text"]]
-
-
 def ext_rt(row):
+    """
+    Function to extract full retweet text
+    For use in txt_to_df and .apply or .map functionality
+
+    Args:
+        row: row from dataframe
+
+    Returns:
+        Full text of a retweeted tweet
+    """
     try:
         if type(row["retweeted_status"]) == dict:
             return row["retweeted_status"]["extended_tweet"]["full_text"]
@@ -80,10 +75,30 @@ def ext_rt(row):
 
 
 def to_year(row):
+    """
+    Function to extract year
+    For use in txt_to_df and .apply or .map functionality
+
+    Args:
+        row: row from dataframe
+
+    Returns:
+        Year a tweet was tweeted
+    """
     return row["year"].year
 
 
 def rm_links(row):
+    """
+    Function to remove links from tweets
+    For use in txt_to_df and .apply or .map functionality
+
+    Args:
+        row: row from dataframe
+
+    Returns:
+        Tweet without links
+    """
     text = row["long_text"]
     text = re.sub(r"https:\S*", "", text)
     row["long_text"] = text
@@ -91,6 +106,19 @@ def rm_links(row):
 
 
 def display_topics(model, feature_names, no_top_words, topic_names=None):
+    """
+    Helps display topics from a given NMF model
+
+    Args:
+        model: NMF model being discovered
+        feature_names: Terms/tokens being used
+            - can use _.get_feature_names() to help
+        no_top_words: Number of topics being discovered
+        topic_names: Optional to name each topic
+
+    Returns:
+        Prints the topics with the corresponding terms/tokens
+    """
     for ix, topic in enumerate(model.components_):
         if not topic_names or not topic_names[ix]:
             print("\nTopic ", ix)
@@ -107,6 +135,17 @@ def display_topics(model, feature_names, no_top_words, topic_names=None):
 
 
 def scatter(x, colors, num_topics):
+    """
+    Function to quickly visualize our TSNE data
+
+    Args:
+        x: Coordinates from TSNE
+        colors: Variables to determine colors
+        num_topics: Number of topics
+
+    Returns:
+        Plots TSNE in a nice scatter plot
+    """
     # We choose a color palette with seaborn.
     palette = np.array(sns.color_palette("hls", num_topics))
 
@@ -127,191 +166,6 @@ def scatter(x, colors, num_topics):
         # Position of each label.
         xtext, ytext = np.median(x[colors == i, :], axis=0)
         txt = ax.text(xtext, ytext, str(i), fontsize=24)
-        # txt.set_path_effects([
-        #     PathEffects.Stroke(linewidth=5, foreground="w"),
-        #     PathEffects.Normal()])
         txts.append(txt)
 
     return f, ax, sc, txts
-
-
-# Let's make visualizing the data a bit easier
-
-# def txt_to_df(txt_path):
-#     """
-#     Powerhouse function to take raw scraped twitter data
-#     into a DataFrame of just the tweet texts
-
-#     Args:
-#         txt_path: The path for the saved file containing
-#             the tweet data in json format
-
-#     Returns:
-#         A DataFrame containing just the raw tweet texts
-#     """
-#     path = txt_path
-#     tweets_file = open(path, "r")
-#     tweets_data = []
-#     for line in tweets_file:
-#         try:
-#             tweet = json.loads(line)
-#             tweets_data.append(tweet)
-#         except:
-#             continue
-#     tweet = pd.DataFrame(tweets_data)
-#     tweet = tweet[tweet["lang"] == "en"]
-#     try:
-#         tweet = tweet[["full_text","created_at","retweeted_status"]]
-#         tweet["long_text"] = tweet["full_text"]
-#         tweet["long_text"] = tweet.apply(ext_rt, axis=1)
-#         tweet["year"] = pd.to_datetime(tweet["created_at"])
-#         tweet["year"] = tweet.apply(to_year, axis=1)
-#         return tweet[["long_text", "year"]]
-#     except:
-#         try:
-#             tweet["long_text"] = tweet['content']
-#             tweet["year"] = pd.to_datetime(tweet["date"])
-#             tweet["year"] = tweet.apply(to_year, axis=1)
-#             return tweet[["long_text", "year"]]
-#         except:
-#             return tweet[["long_text", "year"]]
-
-
-# def snscrape_to_df(txt_path):
-#     path = txt_path
-#     tweets_file = open(path, "r")
-#     tweets_data = []
-#     for line in tweets_file:
-#         try:
-#             tweet = json.loads(line)
-#             tweets_data.append(tweet)
-#         except:
-#             continue
-#     tweet = pd.DataFrame(tweets_data)
-#     tweet = tweet[tweet["lang"] == "en"]
-#     tweet["long_text"] = tweet["content"]
-#     return tweet[["long_text"]]
-
-# def ext_rt(row):
-#     try:
-#         if type(row["retweeted_status"]) == dict:
-#             return row["retweeted_status"]["extended_tweet"]["full_text"]
-#         else:
-#             return row["long_text"]
-#     except:
-#         return row["long_text"]
-
-# def to_year(row):
-#     return row["year"].year
-
-# def rm_links(row):
-#     text = row["long_text"]
-#     text = re.sub(r'https:\S*', '', text)
-#     row["long_text"] = text
-
-
-### PRE YEARS ###
-# def txt_to_df(txt_path):
-#     """
-#     Powerhouse function to take raw scraped twitter data
-#     into a DataFrame of just the tweet texts
-
-#     Args:
-#         txt_path: The path for the saved file containing
-#             the tweet data in json format
-
-#     Returns:
-#         A DataFrame containing just the raw tweet texts
-#     """
-#     path = txt_path
-#     tweets_file = open(path, "r")
-#     tweets_data = []
-#     for line in tweets_file:
-#         try:
-#             tweet = json.loads(line)
-#             tweets_data.append(tweet)
-#         except:
-#             continue
-#     tweet = pd.DataFrame(tweets_data)
-#     tweet = tweet[tweet["lang"] == "en"]
-#     try:
-#         tweet = tweet[
-#             [
-#                 "text",
-#                 "created_at",
-#                 "truncated",
-#                 "extended_tweet",
-#                 "retweeted_status",
-#             ]
-#         ]
-#         tweet["long_text"] = tweet.apply(ext_tweets, axis=1)
-#         tweet["long_text"] = tweet.apply(ext_rt, axis=1)
-#         tweet["year"] = pd.to_datetime(tweet["created_at"])
-#         tweet["year"] = tweet.apply(to_year, axis=1)
-#         return tweet[["long_text", "year"]]
-#     except:
-#         try:
-#             tweet["long_text"] = tweet.apply(ext_rt2, axis=1)
-#             tweet["year"] = pd.to_datetime(tweet["year"])
-#             tweet["year"] = tweet.apply(to_year, axis=1)
-#             return tweet[["long_text"]]
-#         except:
-#             return tweet[["long_text"]]
-
-
-# def snscrape_to_df(txt_path):
-#     path = txt_path
-#     tweets_file = open(path, "r")
-#     tweets_data = []
-#     for line in tweets_file:
-#         try:
-#             tweet = json.loads(line)
-#             tweets_data.append(tweet)
-#         except:
-#             continue
-#     tweet = pd.DataFrame(tweets_data)
-#     tweet = tweet[tweet["lang"] == "en"]
-#     tweet["long_text"] = tweet["content"]
-#     return tweet[["long_text"]]
-
-
-# def ext_tweets(row):
-#     """
-#     Helper function to determined truncated tweets and
-#     then return the full tweet
-
-#     Args:
-#         row: the row a larger DataFrame consisting of all
-#             tweet information
-
-#     Returns:
-#         A new row with the full tweet text
-#     """
-#     if row["truncated"] == True:
-#         return row["extended_tweet"]["full_text"]
-#     else:
-#         return row["text"]
-
-
-# def ext_rt(row):
-#     try:
-#         if type(row["retweeted_status"]) == dict:
-#             return row["retweeted_status"]["extended_tweet"]["full_text"]
-#         else:
-#             return row["long_text"]
-#     except:
-#         return row["long_text"]
-
-
-# def ext_rt2(row):
-#     try:
-#         if type(row["retweeted_status"]) == dict:
-#             return row["retweeted_status"]["full_text"]
-#         else:
-#             return row["full_text"]
-#     except:
-#         return row["full_text"]
-
-
-# def to_year(row):
-#     return row["year"].year
